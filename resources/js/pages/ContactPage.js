b833ad25d80e57bc1cdbd5515fd171f7a3ce1cc5
= <=>
2021-05-13 03:31:24 +0600

    First Commit

--- resources/js/pages/ContactPage.js
+++ resources/js/pages/ContactPage.js
@@ -1,10 +1,14 @@
 import React, {Component, Fragment} from 'react';
+import Loading from '../components/loadingDiv';
+import Error from '../components/wentWrong';
 import {Button} from 'react-bootstrap';
 import SideBar from '../components/SideBar';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 import swal from 'sweetalert';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Axios from 'axios';
 
 
@@ -13,58 +17,81 @@ class ContactPage extends Component {
 		super()
 		this.state = {
 			contactData : [],
+			isLoading : true,
+			isError : false,
+			deleteID : '',
 		}
 	}
 	componentDidMount(){
 		Axios.get('http://127.0.0.1:8000/ContactList')
 		.then(response=>{
-			this.setState({contactData : response.data});
+			this.setState({isLoading:false, isError:false, contactData : response.data});
 		})
 		.catch(error=>{
-
+			this.setState({isLoading:false, isError:true});
 		})
 	}
 	onClick=()=>{
-		swal({
-			  title: "Are you sure?",
-			  text: "Do you want to remove this message!",
+		if(this.state.deleteID===''){
+			swal({
+			  title: "Please select any row",
 			  icon: "warning",
 			  buttons: true,
 			  dangerMode: true,
 			})
-.then((willDelete) => {
-  if (willDelete) {
-    swal("Message has been deleted", {
-      icon: "success",
-    });
-  }
-});
+		}else{
+		swal({
+		  title: "Are you sure?",
+		  text: "Do you want to delete this message?",
+		  icon: "warning",
+		  buttons: true,
+		  dangerMode: true,
+		})
+		.then((willDelete) => {
+			Axios.post('http://127.0.0.1:8000/ContactDelete', {id: this.state.deleteID})
+			.then(response=>{
+					this.componentDidMount();
+					this.setState({deleteID:''})
+			})
+			.catch(error=>{
+				
+			})
+		  if (willDelete) {
+		    swal("Poof! Your imaginary file has been deleted!", {
+		      icon: "success",
+		    });
+		  } 
+		});
+
+}
+
 	}
     render() {
+    	if(this.state.isLoading==true && this.state.isError==false)
+    	{
+    		return <Loading/>
+    	}
+    	else if(this.state.isError==true && this.state.isLoading==false)
+    	{
+    		return <Error/>
+    	}
+    	else{
 		const allData = this.state.contactData;
 
-		const columns = [{
-		  dataField: 'id',
-		  text: 'ID',
-		  sort:true
-		},  
-		{
-		  dataField: 'name',
-		  text: 'Sender Name',
-		  sort:true
-		}, 
-		{
-		  dataField: 'email',
-		  text: 'Sender Email Address'
-		},
-		{
-		  dataField: 'message',
-		  text: 'Message From Sender'
-		}];
+		const columns = [
+		{dataField: 'id',text: 'ID',sort:true},  
+		{dataField: 'name',text: 'Sender Name',sort:true}, 
+		{dataField: 'email',text: 'Sender Email Address'},
+		{dataField: 'message',text: 'Message From Sender'}
+		];
 		const selectRow = {
 		  mode: 'radio',
-		  clickToSelect: false,
+		  onSelect:(row, isSelect, rowIndex)=>{ 
+		  	this.setState({deleteID : row['id']})
+		  }
+		  
 		};
+
         return (
             <Fragment>
                 <SideBar title="Contact">
@@ -80,6 +107,7 @@ class ContactPage extends Component {
                 </SideBar>
             </Fragment>
         );
+     }
     }
 }
 
